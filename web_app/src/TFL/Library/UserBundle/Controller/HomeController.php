dcae2b06f8517c423e814b399d41301c040eae14
Jeff K <admin@thedreamingjeff.com>
2012-08-01 16:42:16 -0700

    * move library table to "homepage" and redirect to /login with welcome message

--- web_app/src/TFL/Library/UserBundle/Controller/HomeController.php
+++ web_app/src/TFL/Library/UserBundle/Controller/HomeController.php
@@ -4,15 +4,29 @@ namespace TFL\Library\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use FOS\UserBundle\Model\UserInterface;
 
 class HomeController extends Controller
 {
 	/**
 	 * @Route("/")
-	 * @Template()
+	 * @Template("TFLLibraryUserBundle:UserLibrary:completeLibrary.html.twig")
 	 */
 	public function homepageAction()
 	{
-		return array();
+		//if not logged in, show form
+		$user = $this->container->get('security.context')->getToken()->getUser();
+		if (!is_object($user) || !$user instanceof UserInterface) {
+			return $this->redirect($this->generateUrl('fos_user_security_login'));
+		}
+		
+		//else displat complete library
+		$em = $this->getDoctrine()->getEntityManager();
+		$query = $em->createQuery(
+			'SELECT DISTINCT b, count(b.id) FROM TFLLibraryBookBundle:BookOwner b WHERE b.isDeleted = :deleted GROUP BY b.bookId'
+		)->setParameter('deleted', '0');
+		$books = $query->getResult();
+		
+		return array('books' => $books);
 	}
 }
